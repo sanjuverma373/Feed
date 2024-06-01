0893d9857dc015ac0596800086d10f421fe95bb4
sanjuverma373 <sv0240866@gmail.com>
2024-06-01 12:33:44 +0530

    bg

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -57,7 +57,7 @@ const Feed = () => {
           </div>
         }
         <div className=' flex items-center justify-center gap-2 pb-4'>
-          <button onClick={() => tabchanges("tab1")} className={` ${first === "tab1" && " !border !border-lightGreen !shadow-[inset_2px_2px_0px_0px_#0C0A0980] !text-darkBrown bg-lightWhite "} shadow-[1px_1px_0px_0px_#CBECD6] w-[98px] h-[32px] flex justify-center items-center text-lightSky2 font-Inter font-normal bg-transparent border border-lightSky rounded text-base leading-[16px] `}>All</button>
+          <button onClick={() => tabchanges("tab1")} className={` ${first === "tab1" && " !border !border-lightGreen !shadow-[inset_2px_2px_0px_0px_#0C0A0980] !text-darkBrown !bg-lightWhite "} shadow-[1px_1px_0px_0px_#CBECD6] w-[98px] h-[32px] flex justify-center items-center text-lightSky2 font-Inter font-normal bg-transparent border border-lightSky rounded text-base leading-[16px] `}>All</button>
           <button onClick={() => tabchanges("tab2")} className={` ${first === "tab2" && " !border !border-lightGreen !shadow-[inset_2px_2px_0px_0px_#0C0A0980] !text-darkBrown !bg-lightWhite "} shadow-[1px_1px_0px_0px_#CBECD6] w-[109px] h-[32px] flex justify-center items-center text-lightSky2 font-Inter font-normal bg-transparent border border-lightSky rounded text-base leading-[16px] `}>Leaderboard</button>
         </div>
         {
